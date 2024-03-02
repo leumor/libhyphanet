9412dcebd7385b92d98745ea9e31e77c7c6043d5
Leumor <116955025+leumor@users.noreply.github.com>
2024-03-02 16:03:33 +0000

    Cleanup code

--- third_party/cppcrypto/alignedarray.h
+++ third_party/cppcrypto/alignedarray.h
@@ -8,166 +8,104 @@ This code is written by kerukuro for cppcrypto library
 
 #include "portability.h"
 #include <algorithm>
-#include <memory.h>
+#include <memory>
 #include <stdlib.h>
 
 namespace cppcrypto {
 template<typename T, size_t N, size_t A> class aligned_pod_array {
 public:
-    aligned_pod_array();
-    ~aligned_pod_array();
-    operator T*() { return t; }
+    aligned_pod_array()
+    {
+        auto size = (sizeof(T) * N / A + 1) * A;
+        t = static_cast<T*>(aligned_allocate(size, A));
+    }
+    ~aligned_pod_array() { reset(); }
+    explicit(false) operator T*() { return t; }
     T* get() { return t; }
     const T* get() const { return t; }
-    void reset();
+    void reset()
+    {
+        if (t) {
+            aligned_deallocate(t);
+            t = nullptr;
+        }
+    }
     size_t size() const { return N; }
     size_t bytes() const { return sizeof(T) * N; }
 
-    aligned_pod_array(aligned_pod_array&& other);
-    aligned_pod_array& operator=(aligned_pod_array&& other);
-    aligned_pod_array(const aligned_pod_array& other);
-    aligned_pod_array& operator=(const aligned_pod_array& other);
-private:
-    T* t;
-};
-
-template<typename T, size_t N, size_t A>
-aligned_pod_array<T, N, A>::aligned_pod_array(): t(0)
-{
-    auto size = (sizeof(T) * N / A + 1) * A;
-    t = static_cast<T*>(aligned_allocate(size, A));
-}
-
-template<typename T, size_t N, size_t A>
-aligned_pod_array<T, N, A>::~aligned_pod_array()
-{
-    reset();
-}
-
-template<typename T, size_t N, size_t A>
-void aligned_pod_array<T, N, A>::reset()
-{
-    if (t) {
-        aligned_deallocate(t);
-        t = nullptr;
+    aligned_pod_array(aligned_pod_array&& other) noexcept: t{other.t}
+    {
+        other.t = nullptr;
     }
-}
 
-template<typename T, size_t N, size_t A>
-aligned_pod_array<T, N, A>::aligned_pod_array(
-    aligned_pod_array<T, N, A>&& other)
-{
-    t = other.t;
-    other.t = nullptr;
-}
+    aligned_pod_array& operator=(aligned_pod_array&& other) noexcept
+    {
+        std::swap(t, other.t);
+        return *this;
+    }
 
-template<typename T, size_t N, size_t A> aligned_pod_array<T, N, A>&
-aligned_pod_array<T, N, A>::operator=(aligned_pod_array<T, N, A>&& other)
-{
-    std::swap(t, other.t);
-    return *this;
-}
+    aligned_pod_array(const aligned_pod_array& other)
+    {
+        t = static_cast<T*>(aligned_allocate(sizeof(T) * N, A));
+        *this = other;
+    }
 
-template<typename T, size_t N, size_t A>
-aligned_pod_array<T, N, A>::aligned_pod_array(
-    const aligned_pod_array<T, N, A>& other)
-    : t(nullptr)
-{
-    t = static_cast<T*>(aligned_allocate(sizeof(T) * N, A));
-    *this = other;
-}
-
-template<typename T, size_t N, size_t A> aligned_pod_array<T, N, A>&
-aligned_pod_array<T, N, A>::operator=(const aligned_pod_array<T, N, A>& other)
-{
-    memcpy(t, other.t, sizeof(T) * N);
-    return *this;
-}
+    aligned_pod_array& operator=(const aligned_pod_array& other)
+    {
+        memcpy(t, other.t, sizeof(T) * N);
+        return *this;
+    }
+private:
+    T* t{nullptr};
+};
 
 template<typename T, size_t A> class aligned_impl_ptr {
 public:
-    aligned_impl_ptr();
-    ~aligned_impl_ptr();
+    aligned_impl_ptr() = default;
+    ~aligned_impl_ptr() { destroy(); }
 
-    template<typename RT> void create();
-    void destroy();
+    template<typename RT> void create()
+    {
+        void* p = aligned_allocate(sizeof(RT), A);
+        t = new (p) RT;
+    }
 
-    template<typename RT, typename P> void create(const P& p);
+    void destroy()
+    {
+        if (t) {
+            t->~T();
+            aligned_deallocate(t);
+            t = nullptr;
+        }
+    }
 
-    operator T*() { return t; }
+    template<typename RT, typename P> void create(const P& param)
+    {
+        void* p = aligned_allocate(sizeof(RT), A);
+        t = new (p) RT(param);
+    }
+
+    explicit(false) operator T*() { return t; }
     T* get() { return t; }
     const T* get() const { return t; }
 
     T* operator->() const { return t; }
 
-    aligned_impl_ptr(aligned_impl_ptr&& other);
-    aligned_impl_ptr& operator=(aligned_impl_ptr&& other);
+    aligned_impl_ptr(aligned_impl_ptr&& other) noexcept: t{other.t}
+    {
+        other.t = nullptr;
+    }
+
+    aligned_impl_ptr& operator=(aligned_impl_ptr&& other) noexcept
+    {
+        std::swap(t, other.t);
+        return *this;
+    }
 private:
     aligned_impl_ptr(const aligned_impl_ptr& other) = delete;
     aligned_impl_ptr& operator=(const aligned_impl_ptr& other) = delete;
-    T* t;
+    T* t{nullptr};
 };
-
-template<typename T, size_t A> aligned_impl_ptr<T, A>::aligned_impl_ptr()
-    : t(nullptr)
-{}
-
-template<typename T, size_t A> template<typename RT>
-void aligned_impl_ptr<T, A>::create()
-{
-    void* p = aligned_allocate(sizeof(RT), A);
-    t = new (p) RT;
-}
-
-template<typename T, size_t A> template<typename RT, typename P>
-void aligned_impl_ptr<T, A>::create(const P& param)
-{
-    void* p = aligned_allocate(sizeof(RT), A);
-    t = new (p) RT(param);
-}
-
-template<typename T, size_t A> void aligned_impl_ptr<T, A>::destroy()
-{
-    if (t) {
-        t->~T();
-        aligned_deallocate(t);
-        t = nullptr;
-    }
-}
-
-template<typename T, size_t A> aligned_impl_ptr<T, A>::~aligned_impl_ptr()
-{
-    destroy();
-}
-
-template<typename T, size_t A>
-aligned_impl_ptr<T, A>::aligned_impl_ptr(aligned_impl_ptr<T, A>&& other)
-{
-    t = other.t;
-    other.t = nullptr;
-}
-
-template<typename T, size_t A> aligned_impl_ptr<T, A>&
-aligned_impl_ptr<T, A>::operator=(aligned_impl_ptr<T, A>&& other)
-{
-    std::swap(t, other.t);
-    return *this;
-}
-
-#if 0
-	template<typename T, size_t A>
-	aligned_impl_ptr<T, A>::aligned_impl_ptr(const aligned_impl_ptr<T, A>& other)
-	{
-		*this = other;
-	}
-
-	template<typename T, size_t A>
-	aligned_impl_ptr<T, A>& aligned_impl_ptr<T, A>::operator=(const aligned_impl_ptr<T, A>& other)
-	{
-		t = other.clone();
-		return *this;
-	}
-#endif
 
 } // namespace cppcrypto
 
