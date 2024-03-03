01938a789a61c3d8a98f840ca550529bc11ccf16
Leumor <116955025+leumor@users.noreply.github.com>
2024-03-03 12:39:24 +0000

    Cleanup code

--- third_party/cppcrypto/rijndael-impl.h
+++ third_party/cppcrypto/rijndael-impl.h
@@ -43,7 +43,7 @@ public:
     void decrypt_blocks(const unsigned char* in, unsigned char* out,
                         size_t n) override;
 private:
-    std::array<__m128i, 30> rk{};
+    __m128i rk[30];
 };
 #endif
 } // namespace cppcrypto::detail
