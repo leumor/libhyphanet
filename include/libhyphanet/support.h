37d4686c78ff0da8d590c14950db1e9b872323a8
Leumor <116955025+leumor@users.noreply.github.com>
2024-08-18 21:01:35 +0100

    use requires to replace enable_if_t

--- include/libhyphanet/support.h
+++ include/libhyphanet/support.h
@@ -69,15 +69,13 @@ namespace concepts {
     template<typename T>
     concept Shared_Ptr = requires(T t) {
         typename T::element_type;
-        typename std::enable_if_t<
-            std::is_same_v<T, std::shared_ptr<typename T::element_type>>>;
+        requires std::is_same_v<T, std::shared_ptr<typename T::element_type>>;
     };
 
     template<typename T>
     concept Unique_Ptr = requires(T t) {
         typename T::element_type;
-        typename std::enable_if_t<
-            std::is_same_v<T, std::unique_ptr<typename T::element_type>>>;
+        requires std::is_same_v<T, std::unique_ptr<typename T::element_type>>;
     };
 
 } // namespace concepts
